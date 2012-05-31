e6f8afa0eb19dd2c1313977b948b7a765de9e9e3
Belden Lyman <belden.lyman@gmail.com>
2012-05-31 15:28:37 -0400

    Add pod

--- lib/antilocal.pm
+++ lib/antilocal.pm
@@ -68,3 +68,79 @@ sub antilocal {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+antilocal.pm - experimental Perl pragma to allow variables to flow *upwards* in the call stack
+
+=head1 SYNOPSIS
+
+Sometimes you really just need safe global variables that only callers earlier than you in the
+call stack may access. antilocal.pm provides that.
+
+=head1 DESCRIPTION
+
+=over 4
+
+  #!/usr/bin/perl
+
+  use strict;
+  use warnings;          # there's nothing up my sleeves
+
+  Somewhere::Else->do_something();
+  my $stashed = antilocal('%stashed');  # go find the antilocal variable named '%stashed'
+	while (my ($key, $value) = each %$stashed) {
+    print "$key => $value\n";
+  }
+
+  exit 0;
+
+  BEGIN {
+    package Somewhere::Else;
+
+    use strict;
+    use warnings;
+
+    sub do_something {
+			my ($class) = @_;
+      $class->prepare;
+    }
+
+    sub prepare {
+			my ($class) = @_;
+      $class->validate;
+    }
+
+    sub validate {
+			my ($class) = @_;
+
+      use antilocal '%stashed';
+      my $stashed = antilocal('%stashed');
+      $stashed->{$_} = $_ + 100 foreach 1..10;
+    }
+  }
+
+=back
+
+=head1 LIMITATIONS
+
+This only handles array and hash references as antilocal-able variables.
+
+=head1 BUGS
+
+Funky code, tragically weird design pattern: you be the judge. Personally, I'm not ready to ship
+this to production.
+
+=head1 AUTHOR
+
+Belden Lyman <belden@cpan.org>
+
+=head1 LICENSE
+
+You may use, distribute, and modify this under the same terms as Perl itself.
+
+=cut
